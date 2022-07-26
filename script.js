751d0fdedd31fbae3c5abfe9e3a076b91da8997e
Your Name <d.hariharan1405@gmail.com>
2022-07-26 14:01:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const billInput = document.getElementById('billInput');
+const tipInput = document.getElementById('tipInput');
+const noOfPeopleDiv = document.getElementById('noOfPeople');
+const perPersonDiv = document.getElementById('perPerson');
+
+let numberOfPeople = Number(noOfPeopleDiv.innerText);
+// console.log(numberOfPeople)
+
+const calculateBill = () => {
+    let bill = Number(billInput.value);
+    // console.log(bill);
+    let tipPercentage = Number(tipInput.value) / 100;
+    // console.log(tipPercentage)
+    let tipAmount = tipPercentage * bill;
+    let totalBill = tipAmount + bill;
+    let perPersonToal = totalBill / numberOfPeople;
+    // console.log(perPersonToal)
+    perPersonDiv.innerText = `₹${perPersonToal.toFixed(2)}`
+}
+
+const increasePeople = () => {
+    numberOfPeople += 1;
+    // console.log(numberOfPeople);
+    noOfPeopleDiv.innerText = numberOfPeople;
+    calculateBill();
+}
+
+const decreasePeople = () => {
+    if(numberOfPeople <=1){
+        return
+    }
+    numberOfPeople -= 1;
+    // console.log(numberOfPeople);
+    noOfPeopleDiv.innerText = numberOfPeople;
+    calculateBill();
+}
